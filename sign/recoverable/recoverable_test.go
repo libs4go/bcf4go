615f3dec903aac78fcee7c0978b754b10bd65ddb
yayanyang <yayanyang@gmail.com>
2021-07-14 14:25:17 +0800

    update

--- sign/recoverable/recoverable_test.go
+++ sign/recoverable/recoverable_test.go
@@ -31,6 +31,21 @@ func TestSignVerify(t *testing.T) {
 	require.Equal(t, publicKey.X, privateKey.PublicKey.X)
 	require.Equal(t, publicKey.Y, privateKey.PublicKey.Y)
 }
+func TestK(t *testing.T) {
+	privateKey, err := ecdsa.GenerateKey(secp256k1.SECP256K1(), rand.Reader)
+
+	require.NoError(t, err)
+
+	sign, err := Sign(privateKey, []byte("hello rfc6979"), false)
+
+	require.NoError(t, err)
+
+	sign1, err := Sign(privateKey, []byte("hello rfc6971119"), false)
+
+	require.NoError(t, err)
+
+	require.NotEqual(t, sign.R, sign1.R)
+}
 
 func TestCompressSignVerify(t *testing.T) {
 	privateKey, err := ecdsa.GenerateKey(secp256k1.SECP256K1(), rand.Reader)
--- sign/rfc6979/rfc6979_test.go
+++ sign/rfc6979/rfc6979_test.go
@@ -23,3 +23,25 @@ func TestSignVerify(t *testing.T) {
 
 	require.True(t, sign.Verfiy(&privateKey.PublicKey, []byte(source)))
 }
+
+func TestK(t *testing.T) {
+	k1, err := ecdsa.GenerateKey(secp256k1.SECP256K1(), rand.Reader)
+
+	require.NoError(t, err)
+
+	k2, err := ecdsa.GenerateKey(secp256k1.SECP256K1(), rand.Reader)
+
+	require.NoError(t, err)
+
+	source := "hello rfc6979"
+
+	s1, err := Sign(k1, []byte(source))
+
+	require.NoError(t, err)
+
+	s2, err := Sign(k2, []byte(source))
+
+	require.NoError(t, err)
+
+	require.NotEqual(t, s1.R, s2.R)
+}
